4a0ae28f8f5c4022091d31cd7d507a9a3ef77cde
LordBreakerX <NathanielH2013@hotmail.com>
2025-04-01 14:03:45 -0400

    updated position utility class
    made some small changes to the documentation and naming of part of the utility class

--- Assets/LordBreakerX/Utilities/Math/PositionUtility.cs
+++ Assets/LordBreakerX/Utilities/Math/PositionUtility.cs
@@ -5,54 +5,56 @@ namespace LordBreakerX.Utilities.Math
     public static class PositionUtility
     {
         /// <summary>
-        /// Generates a random position within a sphere of a specified radius around a starting point.
+        /// Generates a random position within a cube of a specified size centered around a given point.
         /// </summary>
-        /// <param name="size">The radius of the sphere.</param>
-        /// <param name="origin">The center point from which the random position is generated.</param>
-        /// <returns>A random position within the sphere.</returns>
-        public static Vector3 GetRandomPositionInCube(float size, Vector3 origin)
+        /// <param name="halfSize">The half-size of the cube along each axis.</param>
+        /// <param name="origin">The center point of the cube.</param>
+        /// <returns>A random position within the cube.</returns>
+        public static Vector3 GetRandomPositionInCube(float halfSize, Vector3 origin)
         {
-            float offsetX = Random.Range(-size, size);
-            float offsetY = Random.Range(-size, size);
-            float offsetZ = Random.Range(-size, size);
+            float offsetX = Random.Range(-halfSize, halfSize);
+            float offsetY = Random.Range(-halfSize, halfSize);
+            float offsetZ = Random.Range(-halfSize, halfSize);
 
             return origin + new Vector3(offsetX, offsetY, offsetZ);
         }
 
         /// <summary>
-        /// Generates a random position within a disc of a specified radius, oriented by forward and side directions.
+        /// Generates a random position within a square of a specified size, oriented using given direction vectors.
         /// </summary>
-        /// <param name="size">The radius of the disc.</param>
-        /// <param name="origin">The center point of the disc.</param>
-        /// <param name="forward">The forward direction vector of the disc.</param>
-        /// <param name="right">The right (side) direction vector of the disc.</param>
-        /// <returns>A random position within the disc.</returns>
-        public static Vector3 GetRandomPositionInSquare(float size, Vector3 origin, Vector3 forward, Vector3 right)
+        /// <param name="halfSize">The half-size of the square along each direction.</param>
+        /// <param name="origin">The center point of the square.</param>
+        /// <param name="forward">The forward direction vector of the square.</param>
+        /// <param name="right">The right (side) direction vector of the square.</param>
+        /// <returns>A random position within the square.</returns>
+        public static Vector3 GetRandomPositionInSquare(float halfSize, Vector3 origin, Vector3 forward, Vector3 right)
         {
-            float forwardOffset = Random.Range(-size, size);
-            float rightOffset = Random.Range(-size, size);
+            float forwardOffset = Random.Range(-halfSize, halfSize);
+            float rightOffset = Random.Range(-halfSize, halfSize);
 
             Vector3 offset = (forward * forwardOffset) + (right * rightOffset);
             return origin + offset;
         }
 
-        public static Vector3 GetRandomPositionInSquare(float radius, Vector3 origin)
+        public static Vector3 GetRandomPositionInSquare(float halfSize, Vector3 origin)
         {
-            return GetRandomPositionInSquare(radius, origin, Vector3.forward, Vector3.right);
+            return GetRandomPositionInSquare(halfSize, origin, Vector3.forward, Vector3.right);
         }
+
+
 
         /// <summary>
         /// Generates a random position within a half-square area in front of the given origin.
         /// </summary>
-        /// <param name="radius">The maximum distance from the origin along the forward and right directions.</param>
+        /// <param name="halfSize">The maximum distance from the origin along the forward and right directions.</param>
         /// <param name="origin">The starting position.</param>
         /// <param name="forward">The forward direction defining the half-square.</param>
         /// <param name="right">The right direction defining the width of the half-square.</param>
         /// <returns>A random position within the defined half-square area.</returns>
-        public static Vector3 GetRandomPositionInFrontHalfSquare(float radius, Vector3 origin, Vector3 forward, Vector3 right)
+        public static Vector3 GetRandomPositionInFrontHalfSquare(float halfSize, Vector3 origin, Vector3 forward, Vector3 right)
         {
-            float forwardOffset = Random.Range(0, radius);
-            float rightOffset = Random.Range(-radius, radius);
+            float forwardOffset = Random.Range(0, halfSize);
+            float rightOffset = Random.Range(-halfSize, halfSize);
 
             Vector3 offset = (forward * forwardOffset) + (right * rightOffset);
             return origin + offset;
@@ -62,12 +64,13 @@ namespace LordBreakerX.Utilities.Math
         /// Generates a random position within a half-square area in front of the given origin,
         /// assuming the default forward direction as (0, 0, 1) and right direction as (1, 0, 0).
         /// </summary>
-        /// <param name="radius">The maximum distance from the origin along the forward and right directions.</param>
+        /// <param name="halfSize">The maximum distance from the origin along the forward and right directions.</param>
         /// <param name="origin">The starting position.</param>
         /// <returns>A random position within the half-square area.</returns>
-        public static Vector3 GetRandomPositionInFrontHalfSquare(float radius, Vector3 origin)
+        public static Vector3 GetRandomPositionInFrontHalfSquare(float halfSize, Vector3 origin)
         {
-            return GetRandomPositionInFrontHalfSquare(radius, origin, Vector3.forward, Vector3.right);
+            return GetRandomPositionInFrontHalfSquare(halfSize, origin, Vector3.forward, Vector3.right);
         }
+
     }
 }
